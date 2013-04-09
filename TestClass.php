87c6c600be8427fb23ff9572aadc5b4e15dd1293
gituser <nadeera.vista@gmail.com>
2013-04-10 07:03:01 +0800

    adding new class

---
+++ TestClass.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TestClass
+ *
+ * @author nadeera
+ */
+class TestClass {
+    //put your code here
+}
+
+?>
